df059b2e64f3bd25d02cedf56353ab28c6bd2d3f
Tim Farnam <twfarnam@gmail.com>
2017-11-09 13:48:22 -0600

    update branch

---
+++ base.js
@@ -0,0 +1,6 @@
+import { typekit } from '/app/src/utility.js';
+
+// Proxima Nova: regular, italic, bold italic
+
+typekit({kitId: 'jtj0kdc'});
+
---
+++ header.js
@@ -0,0 +1,79 @@
+import * as dom from '/parent/core/dom.js'
+import * as selectors from '/parent/src/selectors.js'
+
+
+export default class Header {
+
+  constructor({ store }) {
+    this.store = store
+    this.store.subscribe(() => this.update())
+    this.update()
+
+    const el = dom.one('body > header')
+    const open = e => el.all('nav').addClass('active')
+    const close = e => el.all('nav').removeClass('active')
+    el.delegate('click', '.open', open)
+    el.delegate('click', '.close, a', close)
+  }
+
+
+  update() {
+    const stream = selectors.stream(this.store)
+    const index = selectors.streamIndex(this.store)
+
+    // only update if there's actually a difference
+    if (this.stream === stream && this.index === index) return
+    this.stream = stream
+    this.index = index
+
+    const paths = selectors.streamPaths(this.store)
+
+    if (!paths || paths.length === 1) {
+      dom.elem('body > header .dots').innerHTML = ''
+    }
+    else {
+      // XXX should slice based on index
+      const dotHTML = paths.slice(0, 11).map((p,i) => {
+        const active = index === i ? 'active' : ''
+        return `
+          <a class="dot ${ active }"
+            href="${ p }"
+            data-in-stream="true">
+          </a>
+        `
+      }).join('') 
+
+      dom.elem('body > header .dots').innerHTML = dotHTML 
+    }
+
+  }
+
+}
+
+
+
+// let lastMax = 0;
+// let lastScroll = 0;
+// let resetDebounced = u.debounce(500, () => lastMax = article.scroll());
+
+// article.on('scroll', () => {
+
+//   let shouldHide = (
+//     article.scroll() > 0 &&
+//     (article.scroll() > lastScroll || article.scroll() > lastMax - 100)
+//   );
+
+//   if (shouldHide)
+//     dom('#dial-menu .bar').addClass('hide')
+//   else
+//     dom('#dial-menu .bar').removeClass('hide')
+
+//   lastScroll = article.scroll();
+
+//   if (article.scroll() > lastMax)
+//     lastMax = article.scroll();
+
+//   resetDebounced();
+// });
+
+
